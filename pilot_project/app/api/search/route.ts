07ad14217a24bf271e953ca98d21239de7af7de4
LelouchOTR <114984033+LelouchOTR@users.noreply.github.com>
2023-12-12 18:22:30 +0100

    deleted search.js which was unused

--- pilot_project/app/api/search/route.ts
+++ pilot_project/app/api/search/route.ts
@@ -13,7 +13,7 @@ const apiConfig = {
   search_settings: {
     search_attributes: ["name", "description"], // Specify the attributes to search on
     result_attributes: ["name", "price", "description", "categories"], // Specify the attributes to return in the results
-    highlight_attributes: ["name"], // Specify the attributes to highlight in the results
+    highlight_attributes: ["name", "price"], // Specify the attributes to highlight in the results
     facet_attributes: [{ attribute: 'price', field: 'price.keyword', type: 'numeric' }], // Specify the attributes to facet on
   },
 };
---
+++ pilot_project/app/components/Hit.tsx
@@ -0,0 +1,14 @@
+import { Highlight } from "react-instantsearch";
+
+const hitView = (props) => {
+  return (
+    <div>
+      <h2>
+        <Highlight hit={props.hit} attribute="name" />
+      </h2>
+      < br />
+
+      <Highlight hit={props.hit} attribute="price" />
+    </div>
+  );
+};
