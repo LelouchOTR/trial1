0edb3ad1e3231914b1dd86e49ce099baaef4d267
ElineGeerits <eline.geerits.eg@gmail.com>
2023-12-22 14:09:15 +0100

    update

---
+++ app/components/index.tsx
@@ -0,0 +1,12 @@
+import FilterComponent from '../components/FilterComponent';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <FilterComponent />
+    </div>
+  );
+};
+
+export default HomePage;
