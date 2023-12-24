14723a6b8cd53e5718656d701e178b1e74797a43
LelouchOTR <114984033+LelouchOTR@users.noreply.github.com>
2023-12-24 04:37:34 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Search() {
           {/* Create a form element */}
           <label
             htmlFor="upload"
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+            className="relative text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
           >
             Upload File{" "}
           </label>
@@ -115,8 +115,14 @@ export default function Search() {
         {/* <RefinementList
           attribute="free_shipping" // Create a RefinementList component for the free_shipping attribute
         /> */}
-        <div> Age {/*Create a div element for the price label*/} </div>
+
+        <label 
+        id="Age" 
+        className="relative text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+        Age
+        </label>
         <RangeInput
+          id="Age"
           attribute="Age" // Create a RangeInput component for the price attribute
           classNames={{
             root: "p-3 shadow-sm",
@@ -130,8 +136,7 @@ export default function Search() {
           hitComponent={hitView} // Create a Hits component with the custom hit component
         />
 
-        <Pagination
-          className="pagination" // Create a Pagination component
+        <Pagination // Create a Pagination component
         />
       </InstantSearch>
     </>
