92acb34a6cc81afb00e9117925104a76b6cd5059
juliaL9 <julialoesekrug@gmail.com>
2024-01-09 13:18:28 +0100

    Rechtschreibfehler verbessert

--- sample_datasets/pictures_server.js
+++ sample_datasets/pictures_server.js
@@ -1,13 +1,14 @@
+
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 9200;
 
-// specify the location for the uploaded data (Spezialisiere den Speicherort für hochgeladene Dateien)
+// specify the location for the uploaded data (Spezifiziere den Speicherort für hochgeladene Dateien)
 const storage = multer.diskStorage({
-  destination: './uploads/',  // folder has to exist (Der Ordner muss existieren)
+  destination: './uploads_pictures/',  // folder has to exist (Der Ordner muss existieren)
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
@@ -16,8 +17,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // terminal for the picture-upload (Endpunkt für den Bild-Upload)
-app.post('/upload', upload.single('image'), (req, res) => {
-  res.json({ message: 'Bild erfolgreich hochgeladen' });
+app.post('/upload_pictures', upload.single('image'), (req, res) => {
+  // the url of the uploaded picture will be here as an output (Hier wird die URL des hochgeladenen Bildes als Antwort gesendet)
+  const imageUrl = `http://localhost:9200/uploads_pictures/${req.file.filename}`;
+  res.json({ imageUrl });
 });
 
 app.listen(port, () => {
