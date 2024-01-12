98ef52fd0867936280074bd4a3d83c95c88f12f3
LelouchOTR <114984033+LelouchOTR@users.noreply.github.com>
2024-01-12 03:27:37 +0100

    cleaned up initial table display

--- app/components/custom-hits.tsx
+++ app/components/custom-hits.tsx
@@ -21,7 +21,7 @@ import buildRowsAndColumns from "../modules/build-rows-and-columns";
 import React, { useState, useEffect } from "react";
 // Define a custom component that renders a table of hits
 function CustomHits(props: UseHitsProps) {
-  const { hits, sendEvent } = useHits(props);
+  const { hits } = useHits(props);
 
   // Define UseState for adding extra Column. Required, otherwise table is not updated
   const [addColumn1, setaddColumn1] = useState(false); // useState is a react function. When an useState const is changed, the table is rerendered.
@@ -48,90 +48,49 @@ function CustomHits(props: UseHitsProps) {
 
   if (hits.length == 0) {
     // If there are no hits, return a table with a single row of empty values with arbitrary column names
-    const hits = [
-      {
-        ID: "-",
-        Age: "-",
-        Weight: "-",
-        Height: "-",
-        Glucose: "-",
-        Cholesterol: "-",
-        "Plasma Glucose Concentration": "-",
-        "Blood Pressure": "-",
-        "Body Fat": "-",
-        Sex: "-",
-        Triglyceride: "-",
-        Symptoms: "-",
-      },
-    ];
-    const columns = [];
-    const column_array = [
-      // Define the arbitrary column names for rendering the empty table
-      "ID",
-      "Age",
-      "Weight",
-      "Height",
-      "Glucose",
-      "Cholesterol",
-      "Plasma Glucose Concentration",
-      "Blood Pressure",
-      "Body Fat",
-      "Sex",
-      "Triglyceride",
-      "Symptoms",
-    ];
-    for (let i = 0; i < column_array.length; i++) {
-      // Build the columns from the column names for NextUI formatting
-      columns.push({ key: column_array[i], label: column_array[i] });
-    }
-
-    const rows = []; // Build the rows array from the arbitrary hits for NextUI formatting
-    for (let i = 0; i < hits.length; i++) {
-      rows.push({
-        key: i,
-        ID: hits[i].ID,
-        Age: hits[i].Age,
-        Weight: hits[i].Weight,
-        Height: hits[i].Height,
-        Glucose: hits[i].Glucose,
-        Cholesterol: hits[i].Cholesterol,
-        "Plasma Glucose Concentration": hits[i]["Plasma Glucose Concentration"],
-        "Blood Pressure": hits[i]["Blood Pressure"],
-        "Body Fat": hits[i]["Body Fat"],
-        Sex: hits[i].Sex,
-        Triglyceride: hits[i].Triglyceride,
-        Symptoms: hits[i].Symptoms,
-      });
-    }
-
-    // Get the hits and the sendEvent function from the useHits hook
-
-    // Return the JSX element that renders the table
-
     return (
-      // Wrap the table inside the NextUIProvider component
       <NextUIProvider>
-        {/* Use the Table component from the NextUI library */}
-        <Table aria-label="Example table with dynamic content" isStriped>
-          <TableHeader columns={columns}>
-            {/* Use a function to map each column object to a TableColumn component */}
-            {(column) => (
-              <TableColumn key={column.key}>{column.label}</TableColumn>
-            )}
-          </TableHeader>
-          <TableBody items={rows}>
-            {(item) => (
-              // Use a function to map each row object to a TableRow component
-              <TableRow key={item.key}>
-                {/* Use a function to map each column key to a TableCell component */}
-                {(columnKey) => (
-                  // Use the getKeyValue function to get the value from the row object by the column key
-                  <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                )}
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+        <div
+          role="status"
+          class="max-w p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
+          <div class="flex items-center justify-between">
+            <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div class="flex items-center justify-between pt-4">
+            <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div class="flex items-center justify-between pt-4">
+            <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div class="flex items-center justify-between pt-4">
+            <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div class="flex items-center justify-between pt-4">
+            <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <span class="sr-only">Loading...</span>
+        </div>
       </NextUIProvider>
     );
   } else {
